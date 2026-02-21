cbb6647c6c8277849abf06a968cdf9b6a2575e4e
Prateek Tandon <ptandon4@wisc.edu>
2026-02-21 12:47:38 -0600

    Receiver finished

--- Minilab 3/receiver.sv
+++ Minilab 3/receiver.sv
@@ -7,21 +7,33 @@ module receiver(
     output reg RDA
 );
 
+    // Counts up to 8 so we know when we have read enough bits
     reg [3:0] counter;
-    reg counter_reset;
-    reg new_RDA;
+    reg counter_reset; // Resets counter back to 0
 
+    // I was getting issues with RDA "blipping" after a successful read.
+    // set_RDA helps control RDA to not glitch.
+    reg set_RDA;
+
+    // Buffer should shift right and counter should increment
     reg shift_enable;
 
+    // Flop for buffer - reset as necessary and then shift the new bit in
     always_ff @(posedge clk)
-        if (reset | counter_reset) receiver_buffer <= 8'hx;
+        if (reset | counter_reset) receiver_buffer <= 8'h00;
         else if (shift_enable) receiver_buffer <= {RxD, receiver_buffer[7:1]};
 
+    // Flop for counter - reset, increment
     always_ff @(posedge clk)
         if (reset | counter_reset) counter <= 4'h0;
         else if (shift_enable) counter <= counter + 1'b1;
 
 
+    // States for the state machine
+    // WAIT is while the receiver waits for transmission to start
+        // (starts as soon as RxD goes down to 0)
+
+    // RECEIVE is when the state machine is reading and shifting data
     typedef enum reg [1:0] {
         WAIT,
         RECEIVE
@@ -29,31 +41,38 @@ module receiver(
 
     state_t state, next_state;
 
+    // State flop
     always_ff @(posedge clk)
         if (reset) state <= WAIT;
         else state <= next_state;
 
+    // RDA flop
+    // Again, set_RDA is used to avoid "blipping" after a successful write.
     always_ff @(posedge clk)
-        if (reset) RDA <= 1'b0;
-        else RDA <= new_RDA;
+        if (reset | counter_reset) RDA <= 1'b0;
+        else if (set_RDA) RDA <= 1'b1; 
 
+
+    // State machine
     always_comb begin
-        next_state = state;
-        new_RDA = RDA;
+        // Default Parameters (avoids latches)
         counter_reset = 1'b0;
         shift_enable = 1'b0;
+        set_RDA = 1'b0;
 
         case (state)
             RECEIVE: begin 
-                new_RDA = (counter == 4'h8);
-                next_state = (counter == 4'h8) ? WAIT : RECEIVE;
-                shift_enable = baud_rate_generator;
+                // Once counter hits 8, wait until baud_rate_generator and then we are done!
+                // Otherwise, we need to increment the counter and wait for the next bit
+                set_RDA = (counter == 4'h8 & baud_rate_generator);
+                next_state = (counter == 4'h8 & baud_rate_generator) ? WAIT : RECEIVE;
+                shift_enable = (counter != 4'h8 & baud_rate_generator);
             end
 
             // Same as WAIT
             default: begin 
+                // Wait for RxD to go low, indicating start of transmission
                 next_state = (~RxD) ? RECEIVE : WAIT;
-                new_RDA = (~RxD) ? 1'b0 : new_RDA;
                 counter_reset = ~RxD;
             end
         endcase
--- Minilab 3/receiver_tb.sv
+++ Minilab 3/receiver_tb.sv
@@ -1,5 +1,6 @@
 module receiver_tb();
 
+    
     reg clk;
     reg reset;
     reg RxD;
@@ -7,17 +8,30 @@ module receiver_tb();
     wire [7:0] receiver_buffer;
     wire RDA;
 
+    // Instantiate DUT
     receiver iDUT(.clk(clk), .reset(reset), .RxD(RxD), .baud_rate_generator(baud_rate_generator), 
                                 .receiver_buffer(receiver_buffer), .RDA(RDA));
 
+    // This will start baud_rate_generator to tick every GENERATOR_RATE seconds
     reg start_generator;
+
+    // Approximate Baud Rate (in ns) - delayed by a few clock cycles as needed
     integer GENERATOR_RATE = 80;
+
+    // Number of communications we will try to send
     integer NUM_ITERATIONS = 22;
 
+    // Used internally for for loops.
+    // They are declared here so we can track them in waveforms.
+    integer iteration, i;
+
+    // Holds the data we are going to transmit
     reg [7:0] data;
 
     initial begin
         $display("Begin Testing");
+
+        // Initial Setup
         clk = 1'b0;
         reset = 1'b1;
         RxD = 1'b1;
@@ -25,30 +39,69 @@ module receiver_tb();
         baud_rate_generator = 1'b0;
         data = 8'h00;
 
+        // Apply values
         @(posedge clk);
+
+        // Deassert reset
         @(negedge clk);
         reset = 1'b0;
         @(posedge clk);
 
+        // Let reset propagate and allow other time as needed
         repeat (10) @(negedge clk);
 
-        for (integer iteration = 0; iteration < NUM_ITERATIONS; iteration = iteration + 1) begin 
+        if (RDA !== 1'b0) begin
+            $display("ERROR (time %0d): RDA was not 0 after initialization", $time);
+        end
+
+        for (iteration = 0; iteration < NUM_ITERATIONS; iteration = iteration + 1) begin 
+            // Start transmission! Changing on negative edge
             @(negedge clk);
+
+            // Start baud_rate_generator
             start_generator = 1'b1;
+
+            // We always start with RxD = 0 according to the SPART spec.
+            // This also triggers receiver to start waiting for a signal
             RxD = 1'b0;
+
+            // Gives data any 8 bit value
             data = $urandom_range(8'h00, 8'hFF);
+
+            // Wait for RxD = 0 to propagate for one baud rate
             @(posedge baud_rate_generator);
-            for (integer i = 0; i < 8; i = i + 1) begin
+
+            for (i = 0; i < 8; i = i + 1) begin
+                // Transmit bit
                 @(negedge clk);
                 RxD = data[i];
                 @(posedge clk);
+
+                @(negedge clk);
+
+                if (RDA !== 1'b0) begin
+                    $display("ERROR (iteration %0d, time %0d): RDA was asserted while we are receiving", iteration, $time);
+                end
+
+                // Wait for baud rate before sending the next bit
                 @(posedge baud_rate_generator);
-                RxD = 1'b1;
             end
-            RxD = 1'b1;
+            repeat (2) @(negedge clk); // Wait enough for data to propagate
+
+            // Check that received data was valid
             if (receiver_buffer !== data) begin
                 $display("ERROR (iteration %0d, time %0d): data was %b but buffer had %b", iteration, $time, data, receiver_buffer);
             end
+            if (RDA !== 1'b1) begin
+                $display("ERROR (iteration %0d, time %0d): RDA was not asserted after receiving", iteration, $time);
+            end
+
+            // Reset RxD back to "IDLE", stop baud_rate_generator
+            RxD = 1'b1;
+            start_generator = 1'b0;
+
+            // Wait arbitrary amount of time before next transmission
+            repeat (20) @(negedge clk);
         end
 
         $display("End Testing");
@@ -57,10 +110,15 @@ module receiver_tb();
 
     always #5 clk = ~clk;
 
+    // Baud Rate Generator
     always begin
         #(GENERATOR_RATE);
+
+        // Once the specified amount of time has passed, start the generator on posedge clk
         @(posedge clk);
         if (start_generator) baud_rate_generator = 1'b1;
+
+        // Assert for 1 cycle
         @(posedge clk);
         baud_rate_generator = 1'b0;
         @(posedge clk);
